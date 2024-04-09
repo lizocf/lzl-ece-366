d831eaf4d2ebbc2da4e4143f57038a0f5ae207c2
Nicholas Storniolo <nicholas.storniolo@cooper.edu>
2024-04-09 16:23:08 -0400

    firebaseConfig added

--- Dupoly-Maven/duopoly/src/index.js
+++ Dupoly-Maven/duopoly/src/index.js
@@ -16,12 +16,20 @@ import CreateAccountPage from "./pages/CreateAccountPage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR INFO HERE"
+    apiKey: "AIzaSyBKN6PPVzL2tRlx29KleJtb9NJgarbx8Dg",
+    authDomain: "duopolytest.firebaseapp.com",
+    projectId: "duopolytest",
+    storageBucket: "duopolytest.appspot.com",
+    messagingSenderId: "562495819262",
+    appId: "1:562495819262:web:49ad78f46dbf4798dc84af",
+    measurementId: "G-GGKTQ7BZQ2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
