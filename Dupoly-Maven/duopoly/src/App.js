3b538b4f4e2c217c1ab34a94cf319ef246b2593b
lizocf <lizelle.ocfemia@cooper.edu>
2024-04-29 13:17:38 -0400

    prepare firebase

--- Dupoly-Maven/duopoly/src/App.js
+++ Dupoly-Maven/duopoly/src/App.js
@@ -9,56 +9,180 @@ import Tiles from "./components/tiles";
 import LeftTables from "./components/lefttables";
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 
+var username = ""; 
+
+const useUser = () => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(getAuth(), user => {
+            setUser(user);
+            setIsLoading(false);
+        });
+
+        return unsubscribe; // if user navigates away, removes hook
+    }, []); // only calls this when auth; only once
+
+    return {user, isLoading};
+};
 
 const LogIn = () => {
+    const [userCredentials, setUserCredentials] = useState({ username: '', password: '' });
+    const login = (username, password) => {
+        setUserCredentials({ username, password});
+    };
+
+    const checkUser = async() => {
+        try {
+            const response = await axios.get('http://localhost:8080/getUserName/' + userCredentials.username);
+        } catch (error) {
+            console.error('checkUser: Error getting user:', error);
+        }
+    };
+
+    const createUser = async () => {
+        try {
+            const response = await axios.post("http://localhost:8080/createNewAccount", {
+            user_name : document.getElementById("new_username").value,
+            user_pw : document.getElementById("new_password").value
+        })
+        setUserCredentials({ username: document.getElementById("new_username").value, password: document.getElementById("new_password").value });
+        return true
+        } catch (error) {
+            if (error.response) {
+                // console.log(error.response.data);
+                // console.log(error.response.status);
+                // console.log(error.response.headers);
+                console.log("This username is already taken. Please try again.")
+                let create_h1 = document.getElementById("create_h1");
+                create_h1.innerHTML = "This username is already taken. Please try again :(";
+            } else if (error.request) {
+                /*
+                 * The request was made but no response was received, `error.request`
+                 * is an instance of XMLHttpRequest in the browser and an instance
+                 * of http.ClientRequest in Node.js
+                 */
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error);
+            return false;
+        }
+    }
+
+
+    function handleLogIn() {
+        var log_in_button = document.getElementById("log_in");
+        var sign_up_button = document.getElementById("sign_up");
+        var create_user_screen = document.getElementById("create_user_screen");
+        var menu_screen = document.getElementById("menu_screen");
+        var login_screen = document.getElementById("login_screen");
+        var go_back_log = document.getElementById("go_back_log");
+        var enter_log = document.getElementById("enter_log");
+        var go_back = document.getElementById("go_back");
+        var enter = document.getElementById("enter");
+
+
+        log_in_button.onclick = function() {
+            login_screen.style.display = "block";
+            menu_screen.style.display = "none";
+        }
+
+        sign_up_button.onclick = function() {
+            create_user_screen.style.display = "block";
+            menu_screen.style.display = "none";
+
+        }
+
+        go_back.onclick = function() {
+            login_screen.style.display = "none";
+            create_user_screen.style.display = "none";
+            menu_screen.style.display = "block";
+            menu_screen.style.flexDirection = "column";
+        }
+
+        enter.onclick = function() {
+            let join_screen = document.getElementById("join_screen");
+            let create_user_screen = document.getElementById("create_user_screen");
+            let welcome_h1 = document.getElementById("welcome_h1");
+            username = document.getElementById("new_username").value;
+            createUser().then((createCheck) => {
+                if (createCheck) {
+                    create_user_screen.style.display = "none";
+                    join_screen.style.display = "flex";
+                    welcome_h1.innerHTML = "Welcome " + username + "!";
+                } else {
+                    console.log("Error creating account");
+                }
+            });
+            console.log("Created Account: " + username + " Password: " + document.getElementById("new_password").value); // I still dont know how useState works :(
+            
+        }
+
+        go_back_log.onclick = function() {
+            login_screen.style.display = "none";
+            create_user_screen.style.display = "none";
+            menu_screen.style.display = "block";
+            menu_screen.style.flexDirection = "column";
+        }
+
+        enter_log.onclick = function() {
+            console.log("Username: " + document.getElementById("username").value + " Password: " + document.getElementById("password").value)
+        }
+    }
+
     function createGame() {
         var create_button = document.getElementById("create_game");
 
         create_button.onclick = function() {
+            var r = (Math.random() + 1).toString(36).substring(4,10);
             axios.post("http://localhost:8080/createNewGame", {
-                game_code : "sdsd" // make this randomized
+                game_code : r // make this randomized
             })
-            console.log('Game has been created.');
+            console.log('Game has been created. Game Code: ' + r);
             // window.location.href = "/game";
         }
 
     }
 
-    // function joinGame() {
-    //     var join_button = document.getElementById("join_game");
-
-    //     create_button.onclick = function() {
-    //         axios.post("http://localhost:8080/createNewGame", {
-    //             game_code : "EEEE"
-    //         })
-    //         console.log('Game has been created.');
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     setGame();
-    //     }, []);
 
     return (
         <div className="login-bg">
             <div className="container_middle">
+            <div className="center" id="menu_screen" style={{margin: "auto auto", display: "flex", flexDirection: "column"}}>
+                    <button type="button" className="button" id="log_in" onClick={() => handleLogIn()} style={{margin: "auto auto", backgroundColor: "#7e5ef4", padding: "1vh 5.4vh"}}> Log In!</button>
+                    <button type="button" className="button" id="sign_up" onClick={() => handleLogIn()}  style={{margin: "1vh auto", padding: "1vh 4.5vh", color:"black"}}> Sign Up!</button>
+            </div>
             <div className="center" style={{margin: "10vh auto"}}>
-            <form>
-                <input className="nickname" type="text" placeholder="Enter nickname..." />
+                <form id="create_user_screen" style={{display: 'none'}}>
+                    <h1 id="create_h1" style={{margin: "1vh auto", fontStyle:"italic"}}>Create Username :)</h1>
+                    <input id="new_username" className="nickname" type="text" placeholder="Enter username..." />
+                    <input  id="new_password" style={{margin: "1vh auto"}}  className="nickname" type="password" placeholder="Enter password..." />
+                    <button type="button" className="button" id="go_back" style={{margin: "-2vh auto", top: "30px", left: "0px", padding: "15px 20px", backgroundColor: "#7e5ef4"}}> Go Back</button>
+                    <button type="button" className="button" id="enter" style={{margin: "-2vh auto", top: "30px", padding: "15px 20px", backgroundColor: "#7e5ef4"}}> Enter</button>
+                </form>
+                <form id="login_screen" style={{display: 'none'}}>
+                    <h1 style={{margin: "1vh auto", fontStyle:"italic"}}>Log In :)</h1>
+                    <input id="username" className="nickname" type="text" placeholder="Enter username..." />
+                    <input  id="password" style={{margin: "1vh auto"}}  className="nickname" type="password" placeholder="Enter password..." />
+                    <button type="button" className="button" id="go_back_log" style={{margin: "-2vh auto", top: "30px", left: "0px", padding: "15px 20px", backgroundColor: "#7e5ef4"}}> Go Back</button>
+                    <button type="button" className="button" id="enter_log" style={{margin: "-2vh auto", top: "30px", padding: "15px 20px", backgroundColor: "#7e5ef4"}}> Enter</button>
                 </form>
             </div>
-            <a href="/game">
-            <button className="button" id="create_game"onClick={() => createGame()} style={{margin: "25vh auto", top: "30px", left: "0px", backgroundColor: "#7e5ef4"}}> Create Game</button>
-            </a>
-            <a href="/game">
-            <button className="button" id="join_game" style={{margin: "25vh auto", top: "30px", padding: "15px 20px"}}> Join Game</button>
-            </a>
+            <div id="join_screen" className="center" style={{margin: "14vh auto", display: "none", flexDirection: "column"}}>
+                    <h1 id="welcome_h1">Welcome!</h1>
+                    <button className="button" id="create_game"onClick={() => createGame()} style={{margin: "auto auto", top: "1vh", backgroundColor: "#7e5ef4"}}> Create Game</button>
+                    <button className="button" id="join_game" style={{margin: "1.5vh auto", top: "1vh"}}> Join Game</button>
+            </div>
             </div>
         </div>
     );
 };
-
 
 const JoinGame = () => {
     return (
